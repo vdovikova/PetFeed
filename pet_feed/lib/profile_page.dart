aac45f87ae06b2739dd3993f47803f35c1c6141b
space pirate <vdovikova.elena@yandex.ru>
2024-04-28 12:06:02 +0300

    Profile page 1.0

--- pet_feed/lib/profile_page.dart
+++ pet_feed/lib/profile_page.dart
@@ -7,144 +7,225 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            snap: true,
-            floating: true,
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    mainYellowColor,
-                    mainGreenColor,
-                  ],
-                ),
-              ),
-            ),
-            centerTitle: true,
-            title: const Text(
-              'PetFeed',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: mainWhiteColor, fontSize: 25, fontFamily: 'Aclonica'),
-            ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: backgroundColor,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'PetFeed',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: mainWhiteColor, fontSize: 25, fontFamily: 'Aclonica'),
           ),
-          const SliverToBoxAdapter(
-            child: SizedBox(height: 20),
-          ),
-          const SliverToBoxAdapter(
-            child: SizedBox(
-              height: 250,
-              width: 100,
-              child: Card(
-                color: mainWhiteColor,
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.edit,
-                          color: textColor,
-                        ),
-                        Text(
-                          "Кокос",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              color: textColor,
-                              fontFamily: "Montserrat",
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 5),
-                    Row(
-                      children: [
-                        Icon(CustomIcons.pawprint, color: textColor),
-                        Text(
-                          "Кот",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              color: textColor,
-                              fontFamily: "Montserrat",
-                              fontSize: 15),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.person, color: textColor),
-                        Text(
-                          "Мальчик",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              color: textColor,
-                              fontFamily: "Montserrat",
-                              fontSize: 15),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.access_time, color: textColor),
-                        Text(
-                          "3 года",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              color: textColor,
-                              fontFamily: "Montserrat",
-                              fontSize: 15),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.medical_information, color: textColor),
-                        Text(
-                          "Кастрирован",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              color: textColor,
-                              fontFamily: "Montserrat",
-                              fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  mainYellowColor,
+                  mainGreenColor,
+                ],
               ),
             ),
           ),
-          SliverList.builder(
-              itemBuilder: (context, index) => Container(
-                    width: double.infinity,
-                    height: 250,
-                    decoration: BoxDecoration(
-                        color: mainYellowColor,
-                        borderRadius: BorderRadius.circular(20)),
-                    margin: const EdgeInsets.symmetric(horizontal: 20)
-                        .copyWith(bottom: 20),
-                  ))
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Питомцы'),
-          BottomNavigationBarItem(
-              icon: Icon(CustomIcons.pawprint), label: 'Главное'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: 'Настройки'),
-        ],
-        selectedLabelStyle: const TextStyle(
-            fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
-        unselectedLabelStyle: const TextStyle(fontFamily: 'Montserrat'),
-        backgroundColor: bottomNavBarColor,
-        unselectedItemColor: textColor,
-        selectedItemColor: mainGreenColor,
+          bottom: const TabBar(
+            labelColor: mainWhiteColor,
+            unselectedLabelColor: textColor,
+            indicatorColor: textColor,
+            labelStyle: TextStyle(
+                fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+            tabs: [
+              Tab(
+                text: "Питомец",
+              ),
+              Tab(
+                text: "Еда",
+              ),
+              Tab(
+                text: "Советы",
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            Container(
+                width: 390,
+                height: 250,
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      color: mainWhiteColor,
+                      child: const Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(children: [
+                          Row(
+                            children: [
+                              Icon(Icons.edit, color: textColor),
+                              Text(
+                                " Кокос",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 30,
+                                    color: textColor),
+                              )
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(4),
+                            child: Row(
+                              children: [
+                                Icon(CustomIcons.pawprint, color: textColor),
+                                Text(
+                                  " Кот",
+                                  style: TextStyle(
+                                      color: textColor,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 15),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(4),
+                            child: Row(
+                              children: [
+                                Icon(Icons.person, color: textColor),
+                                Text(
+                                  " Мальчик",
+                                  style: TextStyle(
+                                      color: textColor,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 15),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(4),
+                            child: Row(
+                              children: [
+                                Icon(Icons.access_time_filled_outlined,
+                                    color: textColor),
+                                Text(
+                                  " 3 года",
+                                  style: TextStyle(
+                                      color: textColor,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 15),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(4),
+                            child: Row(
+                              children: [
+                                Icon(Icons.medication_outlined,
+                                    color: textColor),
+                                Text(
+                                  " Кастрирован",
+                                  style: TextStyle(
+                                      color: textColor,
+                                      fontFamily: 'Montserrat',
+                                      fontSize: 15),
+                                )
+                              ],
+                            ),
+                          ),
+                        ]),
+                      )),
+                )),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(40, 0, 0, 0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.edit,
+                    color: textColor,
+                  ),
+                  Text(
+                    ' Здоровье',
+                    style: TextStyle(
+                        color: textColor,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
+                        fontSize: 25),
+                  )
+                ],
+              ),
+            ),
+            Container(
+                width: 390,
+                height: 210,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 5, 5, 20),
+                  child: Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      color: mainWhiteColor,
+                      child: const Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(children: [
+                          Padding(
+                            padding: EdgeInsets.all(5),
+                            child: Row(children: [
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Активность",
+                                      style: TextStyle(
+                                          fontFamily: 'Montserrat',
+                                          fontSize: 20,
+                                          color: textColor),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ]),
+                          ),
+                          Divider(),
+                          Text(
+                            "Вес",
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 20,
+                                color: textColor),
+                          ),
+                          Divider(),
+                          Text(
+                            "Болезни",
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 20,
+                                color: textColor),
+                          )
+                        ]),
+                      )),
+                )),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Питомцы'),
+            BottomNavigationBarItem(
+                icon: Icon(CustomIcons.pawprint), label: 'Главное'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Настройки'),
+          ],
+          selectedLabelStyle: const TextStyle(
+              fontFamily: 'Montserrat', fontWeight: FontWeight.bold),
+          unselectedLabelStyle: const TextStyle(fontFamily: 'Montserrat'),
+          backgroundColor: bottomNavBarColor,
+          unselectedItemColor: textColor,
+          selectedItemColor: mainGreenColor,
+        ),
       ),
     );
   }
